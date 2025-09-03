ce6f22167e1ba2891f8ec88ccdc405f2949bd5b7
Kirill Yurkin <47507219+ugozapad@users.noreply.github.com>
2025-09-03 23:26:03 +0300

    еще один хак...

--- gamedata/shaders/editor/hud_crosshair.ps.hlsl
+++ gamedata/shaders/editor/hud_crosshair.ps.hlsl
@@ -1,4 +1,9 @@
-#include "common.hlsli"
+struct v2p_TL
+{
+    float2 Tex0 : TEXCOORD0;
+    float4 Color : COLOR;
+    float4 HPos : SV_POSITION; // Clip-space position 	(for rasterization)
+};
 
 // Pixel
 float4 main(v2p_TL I) : SV_Target
